aa51d48590f889fbaa204b5004e932b31f4ca6f0
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-06-02 12:21:10 -0300

    chore: configure database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,10 @@
-import express, { response } from 'express';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  console.log('Listagem de usuários');
+app.use(express.json());
 
-  res.json([
-    'João',
-    'Maria',
-    'Eduarda',
-    'Pedro'
-  ]);
-});
+app.use(routes);
 
 app.listen(3333);

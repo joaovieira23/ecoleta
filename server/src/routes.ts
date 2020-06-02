7dd9d4778a760389ac2e2d76893f7b02c5f48126
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-06-02 14:22:57 -0300

    feat: set collection point register

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,12 +8,52 @@ routes.get('/items', async (req, res) => {
 
   const serializedItems = items.map(item => {
     return {
+      id: item.id,
       name: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`
     }
   })
 
   return res.json(serializedItems);
+});
+
+routes.post('/points', async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = req.body;
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return {
+      item_id,
+      point_id,
+    }
+  })
+
+  await trx('point_items').insert(pointItems);
+
+  return res.json({ success: true });
 });
 
 export default routes;

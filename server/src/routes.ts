c43803de173917b2ad2e91f02315e2572b9d6413
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-06-02 13:41:39 -0300

    feat: set the item listing

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      name: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+    }
+  })
+
+  return res.json(serializedItems);
 });
 
 export default routes;

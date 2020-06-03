fdfa4929c6f208f72edd8e9b1e7137140210e9a0
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-06-03 14:17:25 -0300

    feat: configure navigation

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CreatePoint = () => {
+  return <h1>Create Point</h1>
+}
+
+export default CreatePoint;
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -17,12 +18,12 @@ const Home = () => {
           <h1>Seu marketplace de coleta de resíduos.</h1>
           <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
 
-          <a href="/cadastro">
+          <Link to="/create-point">
             <span>
               <FiLogIn />
             </span>
             <strong>Cadastre um ponto de coleta</strong>
-          </a>
+          </Link>
         </main>
       </div>
     </div>

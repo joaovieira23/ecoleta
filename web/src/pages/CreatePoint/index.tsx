67fa876b32ff2b3ac0fd54e72716e3e1fd7c6988
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-06-03 14:54:49 -0300

    feat: define create-point html

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,123 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
 
 const CreatePoint = () => {
-  return <h1>Create Point</h1>
-}
+  return (
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta"/>
+
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para Home
+        </Link>
+      </header>
+
+      <form>
+        <h1>Cadastro do<br/> ponto de coleta</h1>
+
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Nome da entidade</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+            />
+          </div>
+
+        <div className="field-group">
+          <div className="field">
+            <label htmlFor="email">E-mail</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="whatsapp">WhatsApp</label>
+            <input
+              type="text"
+              name="whatsapp"
+              id="whatsapp"
+            />
+          </div>
+        </div>
+
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
+          </legend>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">Estado (UF)</label>
+              <select name="uf" id="uf">
+                <option value="0">Selecione uma UF</option>
+              </select>
+            </div>
+            <div className="field">
+              <label htmlFor="city">Cidade</label>
+              <select name="city" id="city">
+                <option value="0">Selecione uma cidade</option>
+              </select>
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Ítens de coleta</h2>
+            <span>Selecione um ou mais ítens abaixo</span>
+          </legend>
+
+          <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
+              <span>Lâmpadas</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
+              <span>Lâmpadas</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
+              <span>Lâmpadas</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
+              <span>Lâmpadas</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
+              <span>Lâmpadas</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/lampadas.svg" alt="Lâmpadas"/>
+              <span>Lâmpadas</span>
+            </li>
+          </ul>
+        </fieldset>
+
+        <button type="submit">
+          Cadastrar ponto de coleta
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default CreatePoint;

f5ced0343bb933fec4c7642da87638a9b215dc6c
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-06-04 17:03:49 -0300

    feat: create map page

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
       return {
         id: item.id,
         name: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`
+        image_url: `http://10.0.0.109:3333/uploads/${item.image}`
       }
     })
   

550a0f7240a90f9636a27194ea5dd41e85e5785c
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-06-05 17:08:20 -0300

    feat: add validation with celebrate

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
+
 import multer from 'multer';
 import multerConfig from './config/multer';
 
@@ -17,6 +19,24 @@ routes.get('/items', itemsController.index);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
 
-routes.post('/points', upload.single('image') ,pointsController.create);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required()
+    })
+  }, {
+    abortEarly: false
+  }),
+  pointsController.create
+);
 
 export default routes;
